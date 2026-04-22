3afcbc121bf75249e97bd3caa9062c12ace319de
irwanadinata <irwanadinata9@gmail.com>
2026-04-22 14:10:06 +0700

    feat: create page app

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import Hero from "@/components/sections/Hero";
+import About from "@/components/sections/About";
+import Projects from "@/components/sections/Projects";
+import Skills from "@/components/sections/Skills";
+import Contact from "@/components/sections/Contact";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
